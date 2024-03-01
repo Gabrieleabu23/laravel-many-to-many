68f077ddbe6278901b9dfe7dede4bbe495925993
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-01 07:02:55 +0100

    add: message try in front-end component

--- Front-end/src/App.vue
+++ Front-end/src/App.vue
@@ -1,19 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import TechnologyComp from './components/TechnologyComp.vue'
+export default{
+  components:{
+    TechnologyComp
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ 
+ 
+  <TechnologyComp/>
 </template>
-
 <style scoped>
 .logo {
   height: 6em;
---
+++ Front-end/src/components/TechnologyComp.vue
@@ -0,0 +1,29 @@
+<script >
+    import axios from 'axios';
+export default{
+    name:'TechnologyComp',
+    data(){
+        return{
+            boh:''
+        }
+    },
+    mounted(){
+        axios.get('http://localhost:8000/api/v1/test').
+        then(res=>
+        {
+            this.boh= res.data.message;
+        })
+        .catch(err=> console.log(err));
+
+    }
+}
+</script>
+
+<template>
+<h2>{{ boh }}</h2>
+ 
+</template>
+
+<style scoped>
+
+</style>
